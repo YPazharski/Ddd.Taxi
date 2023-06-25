1e633a3c64c316831527711edda766ffe5091b24
YPazharski <136801415+YPazharski@users.noreply.github.com>
2023-06-25 11:56:29 +0300

    Implemented Equals method that passed tests

--- Infrastructure/ValueType.cs
+++ Infrastructure/ValueType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 
@@ -10,5 +11,36 @@ namespace Ddd.Infrastructure
 	/// </summary>
 	public class ValueType<T>
 	{
-	}
+		static Type ObjType { get; }
+		static PropertyInfo[] ObjProps { get; }
+		static ValueType()
+		{
+            ObjType = typeof(T);
+			ObjProps = ObjType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+			if (obj.GetType() != ObjType) return false;
+			return Equals((T)obj);
+        }
+
+		public bool Equals(T other)
+		{
+            if (other == null) return false;
+            foreach (var prop in ObjProps)
+			{
+				var thisPropValue = prop.GetValue(this);
+				var otherPropValue = prop.GetValue(other);
+				if (thisPropValue == null || otherPropValue == null)
+					return thisPropValue == null && otherPropValue == null ?
+						true
+						: false;
+				if (!thisPropValue.Equals(otherPropValue)) 
+					return false;
+			}
+			return true;
+		}
+    }
 }
